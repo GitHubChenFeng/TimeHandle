f020c7346822d1ac59832269009b172a9b8f0ffd
chenfeng <cf12651951@163.com>
2014-08-01 10:03:35 +0800

    NSDateHandle
    time compare

--- TimeHandle/CFViewController.m
+++ TimeHandle/CFViewController.m
@@ -17,7 +17,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    NSTimeInterval timeInt = [[NSDate date]timeIntervalSince1970];
+    
+    NSDateFormatter* formater = [[NSDateFormatter alloc] init];
+    
+    [formater setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate* date = [formater dateFromString:@"2014-07-31 14:15:24"];
+    
+    NSTimeInterval timeInt2 = [date timeIntervalSince1970];
+
+    CGFloat fr = timeInt2 - timeInt;
+    
+    NSLog(@"===%0.f===%0.f===%0.f",timeInt,timeInt2,fr);
+    
 }
 
 - (void)didReceiveMemoryWarning
